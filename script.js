85e58ce4faa1d414776b845d89a5693ffd70259e
Nishaj <njnishaj0@gmail.com>
2022-12-28 20:05:19 +0530

    completed 2nd enemy's movement

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const canvas = document.querySelector("#canvas1");
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemy = 100;
+const numberOfEnemy = 200;
 const enemiesArray = [];
 
 this.gameFrame = 0;
@@ -11,20 +11,25 @@ this.gameFrame = 0;
 class Enemy {
    constructor() {
       this.image = new Image();
-      this.image.src = 'enemy1.png'
-      // this.speed = Math.random() * 4 - 2;
-      this.spriteWidth = 293;
-      this.spriteHeight = 155;
+      this.image.src = 'enemy2.png'
+      this.speed = Math.random() * 4 + 1;
+      this.spriteWidth = 266;
+      this.spriteHeight = 188;
       this.width = this.spriteWidth / 2.5;
       this.height = this.spriteHeight / 2.5;
       this.x = Math.random() * (canvas.width - this.width);
       this.y = Math.random() * (canvas.height - this.height);
-      this.frame = 0
+      this.frame = 0;
       this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+      this.angle = 0;
+      this.angleSpeed = Math.random() * 0.2
+      this.curve = Math.random() * 7
    }
    update() {
-      this.x += Math.random() * 3 - 1.5;
-      this.y += Math.random() * 3 - 1.5;
+      this.x -= this.speed ;
+      this.y += this.curve * Math.sin(this.angle);
+      this.angle += this.angleSpeed
+      if(this.x + this.width < 0) this.x = canvas.width;
       if (gameFrame % this.flapSpeed === 0) {
          this.frame > 4 ? this.frame = 0 : this.frame++;
       }
